9c368c98d664d73d97903bdc597342fe5aba3edf
ema onaTkh <q826739324@126.com>
2016-11-09 15:12:19 +0800

    Update run.sh

--- zcash/run.sh
+++ zcash/run.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+#!/bin/sh
 zcashd -daemon
 zcash-cli getinfo
 while(( 1==1 ))
 do
-        sleep 3
-        echo `zcash-cli listtransactions`>>/opt/zcash.log
+        sleep 3600
+	nowtime=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
+	echo "<h2>">>/var/www/html/zcash.html
+	echo $nowtime>>/var/www/html/zcash.html
+        echo `zcash-cli listtransactions`>>/var/www/html/zcash.html
+	echo "</h2>">>/var/www/html/zcash.html
+	echo -e "\n">>/var/www/html/zcash.html
+	echo "<br>">>/var/www/html/zcash.html
 done
