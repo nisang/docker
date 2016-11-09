d20f96c5f626c53518c2af120e61087f43282c1b
ema onaTkh <q826739324@126.com>
2016-11-09 23:55:45 +0800

    Update docker-entrypoint.sh

--- zcash/docker-entrypoint.sh
+++ zcash/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "">/root/.zcash/zcash.conf
-echo "addnode=mainnet.z.cash">>/root/.zcash/zcash.conf
+echo "addnode=mainnet.z.cash">/root/.zcash/zcash.conf
 echo "rpcuser=$rpcuser">>/root/.zcash/zcash.conf
 echo "rpcpassword=$rpcpassword">>/root/.zcash/zcash.conf
 echo "gen=1">>/root/.zcash/zcash.conf
 echo "genproclimit=2">>/root/.zcash/zcash.conf
 echo "equihashsolver=tromp">>/root/.zcash/zcash.conf
+
+exec "$@"
