b5bd7b3f87f514eb4e038bfd3f62594793778188
yun <q826739324@126.com>
2016-11-11 23:59:16 +0800

    edit cpu_xenoncat

--- nheqminer/cpu_xenoncat/run.sh
+++ nheqminer/cpu_xenoncat/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-nheqminer -l cn1-zcash.flypool.org:3333 -u $TADDR.$WORKNAME -t $THREADNUM
+cd /opt/nheqminer/Linux_cmake/nheqminer_cpu_xenoncat
+./nheqminer_cpu_xenoncat -l cn1-zcash.flypool.org:3333 -u $TADDR.$WORKNAME -t $THREADNUM
